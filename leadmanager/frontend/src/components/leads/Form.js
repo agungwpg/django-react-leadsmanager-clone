5600af71b057f3105c0fe1b3894809bc123654e6
unknown <agungwpg@gmail.com>
2019-02-03 13:30:04 +0700

    Adding React to the projects, setting up Webpack and Babel

---
+++ leadmanager/frontend/src/components/leads/Form.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Form extends Component {
+  render() {
+    return (
+      <div>
+        <h1>add lead form</h1>
+      </div>
+    )
+  }
+}
